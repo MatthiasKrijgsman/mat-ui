243d73488c34733e094aa9c5c7a4da2dc4f95024
Matthias Krijgsman <Matthiaskrijgsman@gmail.com>
2025-08-22 13:28:50 +0200

    chromatic storybook change test

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -9,6 +9,6 @@ export const Button = (props: ButtonProps) => {
   const { children } = props;
 
   return (
-    <button className={'font-semibold bg-blue-600 text-white px-6 py-3 rounded-lg cursor-pointer transition-all ring-0 hover:ring-4 active:ring-2 hover:bg-blue-700 active:bg-blue-800 ring-blue-200 duration-150'}>{ children }</button>
+    <button className={'font-semibold bg-amber-600 text-white px-6 py-3 rounded-lg cursor-pointer transition-all ring-0 hover:ring-4 active:ring-2 hover:bg-amber-700 active:bg-amber-800 ring-amber-200 duration-150'}>{ children }</button>
   );
 };
