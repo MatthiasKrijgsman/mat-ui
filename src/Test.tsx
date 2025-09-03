d12a82ece79e10618bc2364e765c55a16441d24a
Matthias Krijgsman <Matthiaskrijgsman@gmail.com>
2025-09-03 17:15:56 +0200

    Added (very basic) tooltip

--- src/Test.tsx
+++ src/Test.tsx
@@ -1,17 +1,16 @@
-import { IconFolderFilled } from "@tabler/icons-react";
-import { Badge } from "@/Badge.tsx";
+import { Tooltip } from "@/Tooltip.tsx";
 
 export const Test = () => {
 
   return (
     <div className={ 'flex flex-col gap-3' }>
-      <div className={ 'space-x-2 space-y-2' }>
-        <Badge onClick={ () => {} } color={ 'orange' }>Orange</Badge>
-        <Badge color={ 'orange' }>Orange</Badge>
-        <Badge Icon={ IconFolderFilled } showCloseIcon={true} onClick={ () => {} } color={ 'red' }>Red</Badge>
-        <Badge Icon={ IconFolderFilled } color={ 'red' }>Red</Badge>
-        <Badge showCloseIcon={true} color={ 'red' }>Red</Badge>
-      </div>
+      <p>
+        Lorem ipsum dolor sit amet, <Tooltip content={'Mooi!'}>consectetur adipiscing elit</Tooltip>, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+        laborum.
+      </p>
     </div>
   );
 };
---
+++ src/Tooltip.tsx
@@ -0,0 +1,41 @@
+import * as React from "react";
+import { useState } from "react";
+import { usePopover } from "@/popover/use-popover.tsx";
+import { PopoverPanel } from "@/popover/PopoverPanel.tsx";
+
+export type TooltipProps = {
+  children?: React.ReactNode;
+  content: React.ReactNode;
+  className?: string;
+  position?: 'top' | 'bottom' | 'left' | 'right';
+  delay?: number;
+  maxWidth?: number;
+}
+
+export const Tooltip = (props: TooltipProps) => {
+  const {
+    children,
+    content,
+    className,
+    position = 'top',
+    delay = 300,
+    maxWidth = 200
+  } = props;
+  const [ open, setOpen ] = useState(false);
+  const { anchorRef, Popover } = usePopover({})
+  return (
+    <span
+      ref={ anchorRef }
+      className={ className }
+      onMouseEnter={ () => setOpen(true) }
+      onMouseLeave={ () => setOpen(false) }
+    >
+        { children }
+      <Popover open={ open }>
+        <PopoverPanel>
+          { content }
+        </PopoverPanel>
+      </Popover>
+      </span>
+  );
+};
