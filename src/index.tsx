892c57ce9d1d43c63f5bae2f50fe4b402b6964a4
Matthias Krijgsman <Matthiaskrijgsman@gmail.com>
2025-09-23 10:47:25 +0200

    Added all current components to index

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,53 @@
 import "./style.css";
-import { Button } from "./components/button/Button.tsx";
-import { ButtonIconSquare } from "./components/button-icon-square/ButtonIconSquare.tsx";
-import { ButtonIconRound } from "./components/button-icon-round/ButtonIconRound.tsx";
-import { Input } from "./components/inputs/Input.tsx";
-import { InputCheck } from "./components/inputs/InputCheck.tsx";
-import { InputSelect } from "./components/inputs/InputSelect.tsx";
-import { InputSelectNative } from "./components/inputs/InputSelectNative.tsx";
-import { Panel } from "./components/Panel.tsx";
-import { Divider } from "./components/Divider.tsx";
-import { Test } from "./components/Test.tsx";
 
-export {
-  Button,
-  Input,
-  InputSelectNative,
-  InputSelect,
-  InputCheck,
-  Panel,
-  ButtonIconSquare,
-  ButtonIconRound,
-  Divider,
-  Test
-};
+// Buttons
+export { Button } from "./components/button/Button.tsx";
+export { ButtonIconSquare } from "./components/button-icon-square/ButtonIconSquare.tsx";
+export { ButtonIconRound } from "./components/button-icon-round/ButtonIconRound.tsx";
 
-// Re-export types in a single place
+// Inputs
+export { Input } from "./components/inputs/Input.tsx";
+export { InputPassword } from "./components/inputs/InputPassword.tsx";
+export { InputTextArea } from "./components/inputs/InputTextArea.tsx";
+export { InputRadio } from "./components/inputs/InputRadio.tsx";
+export { InputToggle } from "./components/inputs/InputToggle.tsx";
+export { InputCheck } from "./components/inputs/InputCheck.tsx";
+export { InputLabel } from "./components/inputs/InputLabel.tsx";
+export { InputDescription } from "./components/inputs/InputDescription.tsx";
+export { InputError } from "./components/inputs/InputError.tsx";
+export { InputErrorIcon } from "./components/inputs/InputErrorIcon.tsx";
+export { InputIconButton } from "./components/inputs/InputIconButton.tsx";
+export { InputIconButtonTray } from "./components/inputs/InputIconButtonTray.tsx";
+
+// Selects
+export { InputSelectNative } from "./components/inputs/InputSelectNative.tsx";
+export { InputSelect } from "./components/inputs/InputSelect.tsx";
+export { InputSelectSearchable } from "./components/inputs/InputSelectSearchable.tsx";
+export { InputSelectSearchableAsync } from "./components/inputs/InputSelectSearchableAsync.tsx";
+export { InputSelectOption } from "./components/inputs/InputSelectOption.tsx";
+
+// Layout and feedback
+export { Panel } from "./components/Panel.tsx";
+export { Divider } from "./components/Divider.tsx";
+export { Tooltip } from "./components/Tooltip.tsx";
+export { Modal } from "./components/Modal.tsx";
+export { Badge } from "./components/Badge.tsx";
+export { BadgeColor } from "./components/BadgeColors.tsx";
+export { TabButtons } from "./components/TabButtons.tsx";
+
+// Data display
+export { Spinner } from "./spinner/Spinner.tsx";
+
+// Popover suite
+export { PopoverButton } from "./popover/PopoverButton.tsx";
+export { PopoverPanel } from "./popover/PopoverPanel.tsx";
+export { PopoverBase } from "./popover/PopoverBase.tsx";
+// eslint-disable-next-line react-refresh/only-export-components
+export { usePopover } from "./popover/use-popover.tsx";
+
+// Dev/test components (optional)
+export { Test } from "./components/Test.tsx";
+
+// Re-export all public types
+// eslint-disable-next-line react-refresh/only-export-components
 export * from "./types.ts";
