b70966b0d0f3aad0f10843c07a69fcb684e112d8
Matthias Krijgsman <Matthiaskrijgsman@gmail.com>
2026-05-19 09:12:05 +0200

    Fixed z-index issue

--- site/app/_components/Nav.tsx
+++ site/app/_components/Nav.tsx
@@ -8,7 +8,7 @@ import Link from "next/link";
 export const Nav = () => {
   return (
     <div
-      className={ 'border-b border-stone-200 dark:border-stone-700! h-[70px] sticky top-0 bg-stone-100/50 dark:bg-stone-700/50 backdrop-blur-xl' }>
+      className={ 'border-b border-stone-200 dark:border-stone-700! h-[70px] sticky top-0 bg-stone-100/50 dark:bg-stone-700/50 backdrop-blur-xl z-10' }>
       <div className={ 'w-page-content flex flex-row w-full h-full items-center justify-between' }>
         <div className={ 'flex flex-row items-center gap-3' }>
           <div className={ 'h-8 w-8 rounded-xl bg-black text-white font-bold grid place-items-center' }>M</div>
